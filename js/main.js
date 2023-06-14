025dc1f9cd8c034f4d3763f390ffed955b5c88b2
unexpectedhaki <tpritchard843@gmail.com>
2023-06-14 16:52:23 -0400

    Added War game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,55 @@
 //Example fetch using https://deckofcardsapi.com/
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', drawTwo);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+let deckId = '';
+
+//fetch a deck on page load and store the deck ID
+fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data);
+        deckId= data.deck_id;
+      })
+      .catch(err => {
+          console.log(`error ${err}`);
+      });
+
+
+function drawTwo(){
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
+        console.log(data);
+        document.querySelector('#player1').src = data.cards[0].image;
+        document.querySelector('#player2').src = data.cards[1].image;
+
+        let player1Val = convertToNum(data.cards[0].value);
+        let player2Val = convertToNum(data.cards[1].value);
+        if(player1Val > player2Val) {
+          document.querySelector('h3').innerText = 'Player 1 Wins';
+        } else if (player1Val < player2Val) {
+          document.querySelector('h3').innerText = 'Player 2 Wins';
+        } else {
+          document.querySelector('h3').innerText = 'This means WAR';
+        }
       })
       .catch(err => {
-          console.log(`error ${err}`)
+          console.log(`error ${err}`);
       });
 }
 
+function convertToNum(val) {
+  if (val === 'ACE') {
+    return 14;
+  } else if (val ==='KING') {
+    return 13;
+  } else if (val ==='QUEEN') {
+    return 12;
+  } else if (val ==='JACK') {
+    return 11;
+  } else {
+    return Number(val);
+  }
+}
